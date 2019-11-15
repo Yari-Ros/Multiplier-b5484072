8824e905a849f11752f8bb3d2f594eb0898f5536
Yari-Ros <2118088@talnet.nl>
2019-11-15 13:26:35 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,6 @@
+<?php
+
+$bruh =readline ("I will multiply something for ya, yeah? ");
+$FirstNumber =readline ("Okay now, give a number so we can get to work quick. ");
+$Secondnumber =readline ("Hmm okay now I will need you to give me another number to multiply the first number with.");
+echo "Okay.. gimme a second to let that sink in... AH! I've figured it out. It's " .($FirstNumber*$Secondnumber);
